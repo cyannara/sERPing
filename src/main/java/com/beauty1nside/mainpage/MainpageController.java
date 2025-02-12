86663cdc42225b8907bd788d49bef8a1b9b9da09
Dayoung <kimdayoung771@gmail.com>
2025-02-12 18:40:27 +0900

    김다영 / 시큐리티 구현중 / 20250211

--- src/main/java/com/beauty1nside/mainpage/MainpageController.java
+++ src/main/java/com/beauty1nside/mainpage/MainpageController.java
@@ -1,16 +1,27 @@
 package com.beauty1nside.mainpage;
 
+import com.beauty1nside.security.service.CustomerUser;
+import com.beauty1nside.security.util.SecurityUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Log4j2
 @Controller
 @AllArgsConstructor
 public class MainpageController {
+  @Autowired
+  HttpSession session;
+  
   @GetMapping("/")
   public String main() {
+    CustomerUser userDetails = SecurityUtil.getCustomerUser(); // 공통 유틸리티 사용
+    log.info("userDetails: {}", userDetails.getUserDTO().getDeptName());
+    log.info("session: {}", session.getAttribute("deptName"));
+    
 //    todo-dy
     // token 없으면 login 페이지로 이동하기
 //    return "/login/login";
