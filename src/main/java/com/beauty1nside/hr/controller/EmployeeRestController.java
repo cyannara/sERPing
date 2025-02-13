aad088cc9e0ab795e6aa59a69c8213b5404810d3
seojin <admin@DESKTOP-U820E4V>
2025-02-13 19:09:28 +0900

    배서진 / 페이징 하다가 되돌림 / 20250213

--- src/main/java/com/beauty1nside/hr/controller/EmployeeRestController.java
+++ src/main/java/com/beauty1nside/hr/controller/EmployeeRestController.java
@@ -3,7 +3,6 @@ package com.beauty1nside.hr.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.beauty1nside.hr.dto.EmployeeDTO;
 import com.beauty1nside.hr.service.EmployeeService;
@@ -19,7 +19,7 @@ import groovy.util.logging.Log4j2;
 import lombok.AllArgsConstructor;
 
 @Log4j2
-@Controller
+@RestController
 @AllArgsConstructor
 @RequestMapping("/hr/rest/*")
 public class EmployeeRestController {
--- src/main/java/com/beauty1nside/hr/service/Impl/EmployeeServiceImpl.java
+++ src/main/java/com/beauty1nside/hr/service/Impl/EmployeeServiceImpl.java
@@ -26,6 +26,7 @@ public class EmployeeServiceImpl implements EmployeeService{
 		public List<EmployeeDTO> getAllEmployees() {
 			return employeeMapper.getAllEmployees();
 		}
+		
 
 		@Override
 		@Transactional
