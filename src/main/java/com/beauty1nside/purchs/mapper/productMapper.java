c39dcef2a4def86660563f6c4e3df383a5f6d444
yunngna <nayunkwon124@gmail.com>
2025-02-17 00:07:18 +0900

    권나윤 제품등록모달작업_20250217

--- src/main/java/com/beauty1nside/purchs/mapper/productMapper.java
+++ src/main/java/com/beauty1nside/purchs/mapper/productMapper.java
@@ -13,6 +13,14 @@ public interface productMapper {
 	List<ProductDTO>brandlist(ProductSearchDTO dto);
 	int brandcount(ProductSearchDTO dto);
 	
+	//거래처 조회 (모달)
+	List<ProductDTO>vendorlist(ProductSearchDTO dto);
+	int vendorcount(ProductSearchDTO dto);
+	
+	//창고조회 (모달)
+	List<ProductDTO>warehouselist(ProductSearchDTO dto);
+	int warehousecount(ProductSearchDTO dto);
+	
 	//상품,옵션 등록 
 	int insert(ProductDTO productDTO);
 }
