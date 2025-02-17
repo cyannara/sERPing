965009bffa8d7f00f66224378405ace317a941ad
Hayeon <Hayeon@DESKTOP-F0MAH1V>
2025-02-18 08:01:30 +0900

    표하연 / 결제단 올림 / 자꾸 템플릿이 날 막음 / 202502180801

--- src/main/java/com/beauty1nside/erp/controller/ErpUserRestController.java
+++ src/main/java/com/beauty1nside/erp/controller/ErpUserRestController.java
@@ -1,7 +1,11 @@
 package com.beauty1nside.erp.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,19 +30,21 @@ import lombok.extern.log4j.Log4j2;
  *   수정일      수정자          수정내용
  *  -------    --------    ---------------------------
  *  2025.02.17  표하연          최초 생성
+ *  2025.02.18  표하연          결제 모듈 등록
  *
  *  </pre>
 */
 @Log4j2	//log4j 가 안되면 버전높은 log4j2 사용
 @RestController
-@AllArgsConstructor
+//@AllArgsConstructor
 @RequestMapping("/erp/rest/*")
 public class ErpUserRestController {
 	
 	/**
      * ErpUserService 서비스를 사용하기 위한 의존성 주입
      */
-	private final ErpUserService erpUserService;
+	@Autowired
+	private ErpUserService erpUserService;
 	
 	/**
      * 고객 구독 정보를 받아옴
@@ -63,5 +69,28 @@ public class ErpUserRestController {
 		log.info("bbbbbbb "+compnayNum);
 		return erpUserService.hrlist(compnayNum);
 	}
-
+	
+	/**
+     * 프로퍼티에서 결제키를 가져온다
+     *
+     */
+	private final String impKey;
+	public ErpUserRestController(
+            ErpUserService erpUserService, 
+            @Value("${portone.api.key}") String impKey) {
+        this.erpUserService = erpUserService;
+        this.impKey = impKey;
+    }
+	/**
+     * 결제키를 반환 해준다
+     *
+     * @return Map<String, String>
+     */
+	@GetMapping("/get-imp-key")
+	public Map<String, String> getImpKey() {
+        Map<String, String> response = new HashMap<>();
+        response.put("impKey", impKey);
+        return response;
+    }
+	
 }
