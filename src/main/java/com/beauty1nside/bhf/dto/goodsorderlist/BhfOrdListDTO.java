aaf35e4b788b22b1711b72b1245e23ee20de1ad4
Soonbum <shsrhdtnsqja@naver.com>
2025-02-17 19:01:12 +0900

    권순범/발주요청완료 및 발주조회화면구성/2025.02.17

---
+++ src/main/java/com/beauty1nside/bhf/dto/goodsorderlist/BhfOrdListDTO.java
@@ -0,0 +1,24 @@
+package com.beauty1nside.bhf.dto.goodsorderlist;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class BhfOrdListDTO {
+
+	private String orderCode;
+	private String branchOfficeId;
+	private Date orderDate;
+	private Date dueDate;
+	private String progressStatus;
+	private String remark;
+	
+	private String goodsCode;
+	private String goodsName;
+	private String optionCode;
+	private String optionName;
+	private String goodsStandard;
+	private int quantity;
+	
+}
---
+++ src/main/java/com/beauty1nside/bhf/service/impl/BhfOrderListServiceImpl.java
@@ -0,0 +1,37 @@
+package com.beauty1nside.bhf.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.beauty1nside.bhf.dto.goodsorderlist.BhfOrdListDTO;
+import com.beauty1nside.bhf.dto.goodsorderlist.BhfOrdListSearchDTO;
+import com.beauty1nside.bhf.mapper.BhfOrderListMapper;
+import com.beauty1nside.bhf.service.BhfOrderListService;
+
+@Service
+public class BhfOrderListServiceImpl implements BhfOrderListService {
+	
+	@Autowired
+	private BhfOrderListMapper mapper;
+
+	@Override
+	public List<BhfOrdListDTO> orderList(BhfOrdListSearchDTO dto) {
+		
+		return mapper.orderList(dto);
+	}
+
+	@Override
+	public int count(BhfOrdListSearchDTO dto) {
+		
+		return mapper.count(dto);
+	}
+
+	@Override
+	public List<BhfOrdListDTO> orderDetailList(BhfOrdListSearchDTO dto) {
+		
+		return mapper.orderDetailList(dto);
+	}
+
+}
