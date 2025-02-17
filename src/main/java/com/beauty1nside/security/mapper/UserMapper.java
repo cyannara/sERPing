1b99f5a9fd7d8ddd886986e6d4a61e7a2912bedb
Dayoung <kimdayoung771@gmail.com>
2025-02-17 11:38:14 +0900

    김다영 / 안쓰는 코드 삭제 / 20250217

--- src/main/java/com/beauty1nside/security/mapper/UserMapper.java
+++ src/main/java/com/beauty1nside/security/mapper/UserMapper.java
@@ -1,11 +1,7 @@
 package com.beauty1nside.security.mapper;
 
-import com.beauty1nside.security.service.RoleDTO;
 import com.beauty1nside.security.service.UserDTO;
-
-import java.util.List;
 
 public interface UserMapper {
   UserDTO getUser(String employeeId);
-  List<RoleDTO> getRole(Long employeeNum);
 }
