5f54eb097b340599eb26c64215b20b699481166f
MyBFMoti <jiny5388@gmail.com>
2025-02-18 16:59:10 +0900

    이신화/주문등록프로시저생성및프론트작업중/20250218

--- src/main/java/com/beauty1nside/bsn/service/BsnOrderService.java
+++ src/main/java/com/beauty1nside/bsn/service/BsnOrderService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.beauty1nside.bsn.dto.BhfOrderDTO;
 import com.beauty1nside.bsn.dto.BhfOrderDetailDTO;
+import com.beauty1nside.bsn.dto.BsnOrderDTO;
 import com.beauty1nside.bsn.dto.OrderSearchDTO;
 
 public interface BsnOrderService {
@@ -14,8 +15,12 @@ public interface BsnOrderService {
 	//발주 주문수 구하기
 	public int getCountOfBhfOrder(OrderSearchDTO orderSearchDTO);
 	
+	//발주 주문 상세정보 조회
 	public List<BhfOrderDetailDTO> getBhfOrderDetail(BhfOrderDTO bhfOrderDTO);
 	//발주 상세의 상품수 구하기
 	public int getCountOfBhfOrderDetail(BhfOrderDTO bhfOrderDTO);
+	
+	//주문 등록
+	public void registerOrder(BsnOrderDTO bsnOrederDTO);
 	
 }
