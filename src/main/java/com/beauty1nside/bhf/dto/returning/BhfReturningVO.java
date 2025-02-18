f803a558784ad49b1244f0c68889cdafe8b2f37d
Soonbum <shsrhdtnsqja@naver.com>
2025-02-19 00:58:03 +0900

    권순범/토큰설정및오라클핸들러생성/2025.02.19

--- src/main/java/com/beauty1nside/bhf/dto/returning/BhfReturningVO.java
+++ src/main/java/com/beauty1nside/bhf/dto/returning/BhfReturningVO.java
@@ -10,6 +10,6 @@ public class BhfReturningVO {
 	private String branchOfficeId;
 	private String remark;
 	private int companyNum;
-	private List<BhfReturningDtlVO> files; //반품 상세 테이블에 해당하는 값들을 배열로 담음
+	private List<BhfReturningDtlVO> refiles; //반품 상세 테이블에 해당하는 값들을 배열로 담음
 	
 }
