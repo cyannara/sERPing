4ead1df6bd37c5712f932e6eb3c1fb396bc02f1b
Soonbum <shsrhdtnsqja@naver.com>
2025-02-20 14:51:09 +0900

    권순범/상품입고페이지두번째/2025.02.20

--- src/main/java/com/beauty1nside/bhf/mapper/BhfGoodsInMapper.java
+++ src/main/java/com/beauty1nside/bhf/mapper/BhfGoodsInMapper.java
@@ -2,13 +2,21 @@ package com.beauty1nside.bhf.mapper;
 
 import java.util.List;
 
-import org.springframework.data.repository.query.Param;
-
 import com.beauty1nside.bhf.dto.goodsin.BhfGoodsInVO;
+import com.beauty1nside.bhf.dto.goodsin.BhfGoodsOrdDTO;
+import com.beauty1nside.bhf.dto.goodsin.BhfGoodsOrdSearchDTO;
 
 public interface BhfGoodsInMapper {
 
 	 // 상품 입고 - List<BhfGoodsInVO> 데이터를 받아 MERGE 실행
-    int goodsIn(@Param("list") List<BhfGoodsInVO> goodsList);
+    int goodsIn(List<BhfGoodsInVO> goodsList);
+    
+    // 발송완료인 주문서 조회
+    List<BhfGoodsOrdDTO> goodsOrdList(BhfGoodsOrdSearchDTO dto);
+    
+    int count(BhfGoodsOrdSearchDTO dto);
+    
+    // 주문서 상세 조회
+    List<BhfGoodsOrdDTO> goodsOrdDtlList(BhfGoodsOrdSearchDTO dto);
 	
 }
