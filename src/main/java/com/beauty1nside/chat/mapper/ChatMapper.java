d3e315195827ddfb73b2269b02eaece2b058c904
Dayoung <kimdayoung771@gmail.com>
2025-02-28 14:21:56 +0900

    김다영 / 메신저 > 채팅방 여는 로직 수정 / 20250228

--- src/main/java/com/beauty1nside/chat/mapper/ChatMapper.java
+++ src/main/java/com/beauty1nside/chat/mapper/ChatMapper.java
@@ -9,7 +9,8 @@ import java.util.List;
 public interface ChatMapper {
   List<EmpDTO> empList(Long companyNum);
   RoomDTO getChatRoom(RoomDTO roomDTO);
-  Long insertChatRoom(RoomDTO roomDTO);
+  void insertChatRoom(RoomDTO roomDTO);
+  RoomDTO getRoomJustMade();
   List<MessageDTO> getMsgList(Long roomId);
   void insertMsg(MessageDTO messageDTO);
   MessageDTO getMsgJustSent(Long roomId);
